b372756544044e59037a33cacd126fc85251d3f9
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-18 17:40:31 +0300

    add task 4

---
+++ 0x14-mysql/replica_user.sql
@@ -0,0 +1,4 @@
+-- Creates a replica user
+CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'billyangu';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
