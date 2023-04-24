636422ec21294fbc4f08e93594d68ba988a63d47
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-24 12:37:55 +0300

    corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,38 +7,35 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    if len(sys.argv) == 1:
-        print("Usage: ./0-gather_data_from_an_API.py <employeeID>")
-    else:
-        employeeID = sys.argv[1]
-        url = "https://jsonplaceholder.typicode.com/users/{}"
-        url = url.format(employeeID)
+    employeeID = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    url = url.format(employeeID)
 
-        employee_name = ""
-        number_of_done_tasks = 0
-        total_number_of_tasks = 0
-        completed_task_titles = []
+    employee_name = ""
+    number_of_done_tasks = 0
+    total_number_of_tasks = 0
+    completed_task_titles = []
 
-        with requests.get(url) as res:
-            json = res.json()
-            employee_name = json.get("name")
+    with requests.get(url) as res:
+        json = res.json()
+        employee_name = json.get("name")
 
-        url = "https://jsonplaceholder.typicode.com/todos?userId={}"
-        url = url.format(employeeID)
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    url = url.format(employeeID)
 
-        with requests.get(url) as res:
-            todos = res.json()
-            total_number_of_tasks = len(todos)
-            for item in todos:
-                if item.get("completed") is True:
-                    number_of_done_tasks += 1
-                    completed_task_titles.append(item.get("title"))
+    with requests.get(url) as res:
+        todos = res.json()
+        total_number_of_tasks = len(todos)
+        for item in todos:
+            if item.get("completed") is True:
+                number_of_done_tasks += 1
+                completed_task_titles.append(item.get("title"))
 
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            number_of_done_tasks,
-            total_number_of_tasks
-            ))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name,
+        number_of_done_tasks,
+        total_number_of_tasks
+        ))
 
-        for title in completed_task_titles:
-            print("\t {}".format(title))
+    for title in completed_task_titles:
+        print("\t {}".format(title))
