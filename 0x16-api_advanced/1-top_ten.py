ec06148d46135a1b0e74bf07b8a1d65868afe256
MwauratheAlex <mwaurathealex@gmail.com>
2023-05-11 11:41:17 +0300

    add 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,13 @@ import requests
 def top_ten(subreddit):
     """queries the Reddit API and prints the titles of the first 10
     hot posts listed for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
-    headers = {"User-Agent": "alx"}
 
-    res = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "alx"}
+    params = {"limit": 10}
+
+    res = requests.get(
+            url, headers=headers, params=params, allow_redirects=False)
 
     if res.status_code == 404:
         print(None)
@@ -17,5 +20,5 @@ def top_ten(subreddit):
 
     posts = res.json()["data"]["children"]
 
-    for post in posts[: 10]:
+    for post in posts:
         print(post["data"]["title"])
