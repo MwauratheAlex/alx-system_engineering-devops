d9be02d699ba52441c6346847cbde72de00d3352
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-08 13:14:50 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,21 +6,18 @@ import requests
 def recurse(
         subreddit, hot_list=[],
         params={"after": None, "count": 0, "limit": 100}):
-
     """queries the Reddit API.
         returns: list of titles of all hot articles for subreddit.
         If no results are found for the given subreddit, return None."""
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "alx /0.0.3"}
-
     res = requests.get(
             url, headers=headers, params=params, allow_redirects=False)
     if not res.ok:
         return None
     if res.status_code != 200:
         return None
-
     data = res.json()["data"]
     posts = data["children"]
     titles = [post["data"]["title"] for post in posts]
