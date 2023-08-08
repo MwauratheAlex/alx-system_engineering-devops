9ad1bc97ce3eac9538b33296c793d220c10b2e67
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-08 11:13:48 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""This module contains a function that queries the reddit api"""
 
+
+import requests
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "alx /0.0.2"}
+
+    res = requests.get(url, headers=headers)
+    if not res.ok:
+        print("None")
+        return
+
+    posts = res.json().get("data").get("children")[:10]
+
+    for post in posts:
+        print(post["data"]["title"])
