0f3f2ed96aafb0354f89df1eb625720c04f0757f
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-08 10:47:33 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""queries the Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API
+        returns: number of subscribers for a given subreddit.
+        If an invalid subreddit is given, return 0 """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "alx/0.0.1"}
+
+    res = requests.get(url, headers=headers)
+
+    if not res.ok:
+        return 0
+
+    json_data = res.json()
+    subscribers = json_data.get("data").get("subscribers")
+
+    return subscribers
