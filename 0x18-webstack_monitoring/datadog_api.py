2d9e22a3bbd4a273b7f5b8c4adfacf1a65cc954b
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-09 11:32:21 +0300

    datadog api: remove keys from code

--- 0x18-webstack_monitoring/datadog_api.py
+++ 0x18-webstack_monitoring/datadog_api.py
@@ -8,7 +8,13 @@ from datadog_api_client.v1.api.hosts_api import HostsApi
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = HostsApi(api_client)
-    response = api_instance.list_hosts()
 
-    print(response["host_list"])
+    try:
+        response = api_instance.list_hosts()
+        print(response["host_list"])
+    except Exception as e:
+        message = '''DD_SITE="datadoghq.com" DD_API_KEY="<YOUR_API_KEY>"'''
+        message += ''' DD_APP_KEY="<YOUR_APP_KEY" python3 datadog_api.py'''
 
+        print("Usage: < {} >\n".format(message))
+        print("{}".format(e))
